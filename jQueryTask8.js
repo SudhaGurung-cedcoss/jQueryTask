d393c52263cbf90c5de845c17f07065b413e1e22
SudhaGurung-cedcoss <sudhakumarigurung@cedcoss.com>
2022-08-06 18:54:25 +0530

    Task 8

---
+++ jQueryTask8.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    // jQuery methods go here...
+    $("#DuffleSel").click(function(){
+       $v=$(this).val();
+       if($v=="small"){
+        $("#td1").html("1000 /-");
+       }
+       if($v=="medium"){
+        $("#td1").html("2000 /-");
+       }
+
+       if($v=="Large"){
+        $("#td1").html("3000 /-");
+       }
+    })
+
+
+    $("#T-shirtsSel").click(function(){
+        $v=$(this).val();
+        if($v=="lightgrey"){
+         $("#td2").html("100 /-");
+        }
+        if($v=="blue"){
+         $("#td2").html("200 /-");
+        }
+ 
+        if($v=="black"){
+         $("#td2").html("300 /-");
+        }
+     })
+  
+  });
