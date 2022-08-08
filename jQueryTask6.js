409557a80db87280e6ed144750ec87e9ea69e15a
SudhaGurung-cedcoss <sudhakumarigurung@cedcoss.com>
2022-08-08 17:21:32 +0530

    Task 6

---
+++ jQueryTask6.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $("#btnAdd").click(function(){
+   
+        $("#mainDiv").append("<div ><input placeholder='Enter Class'><input placeholder='Enter board'><input placeholder='Enter marks'><input placeholder='Enter division'><button id='btn11'>+</button ><button id='btn12'>-</button></div>");
+    })
+
+    
+    
+    $("#btnSub").click(function(){
+        $("#innerDiv").remove();
+    })
+    
+
+    $("#mainDiv").on("click","#btn11",function(){
+       $(this).parent().append("<div><input placeholder='Enter Class'><input placeholder='Enter board'><input placeholder='Enter marks'><input placeholder='Enter division'><button id='btn11'>+</button ><button id='btn12'>-</button></div>");
+   })
+
+
+
+   $("#mainDiv").on("click","#btn12",function(){  
+    $(this).parent().remove();
+   })
+   
+
+})
