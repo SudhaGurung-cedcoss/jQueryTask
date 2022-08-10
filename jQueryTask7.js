08e689175d8af539346f3821b69275dcfda6072c
SudhaGurung-cedcoss <sudhakumarigurung@cedcoss.com>
2022-08-10 17:34:41 +0530

    task 7  modified

---
+++ jQueryTask7.js
@@ -0,0 +1,26 @@
+
+var arrFlower=["Rose","Lily","Lavender","Lotus","Jasmin","Sunflower","Daisy","Tulip","Magnolia"];
+
+$(document).ready(function(){
+   
+    $("#flower").keyup(function(){
+         $matchedItem="";
+       for(i=0;i<arrFlower.length;i++){
+      
+  
+         $flower_input=$("#flower").val();
+        
+         $match=arrFlower[i].substring(0,$flower_input.length);
+        
+        
+         if($flower_input==$match){
+           $matchedItem+=" "+arrFlower[i];
+         
+         
+           }
+           $("#suggestion").html("Suggestion :"+$matchedItem);  
+     
+          }
+    })
+   
+})
